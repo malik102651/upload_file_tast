41c978ac5d47dd9d1938afc9c31cfb463b21c306
Malik Naseer <maliknaseer102651@gmail.com>
2022-07-22 16:33:13 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ connectDB()
 app.use(express.json())
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    //origin: 'https://react-node-note-app1.herokuapp.com',
+    //origin: 'http://localhost:3000',
+    origin: 'https://upload-file-task.herokuapp.com/',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
