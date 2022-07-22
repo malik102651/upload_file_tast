af705b8a87a3b2e105109f72a9719923e361cc15
Malik Naseer <maliknaseer102651@gmail.com>
2022-07-22 17:58:50 +0500

    jfjf

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,9 @@ app.use(express.json())
 
 const corsOptions = {
     //origin: 'http://localhost:3000',
-    origin: 'https://upload-file-task.herokuapp.com',
+    credentials: true,
+    origin: "*",
+    //origin: 'https://upload-file-task.herokuapp.com',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
